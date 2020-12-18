ab8ad283df4b1c0619a376f6c703b5e21f510d58
syjung98 <64826099+syjung98@users.noreply.github.com>
2020-12-18 09:24:11 +0900

    Create practice-checkpoint.py

---
+++ .ipynb_checkpoints/practice-checkpoint.py
@@ -0,0 +1,26 @@
+print("hello world")
+
+print("CG made changes")
+print("Pull request")
+
+
+# 정민 수정사항
+print("JM fork Compas_base")
+print("COMMIT!!!!")
+
+print("haha")
+
+print("CG changed again")
+
+print("So hard!")
+
+print("정민이 수정 받고 창균 수정")
+
+print("i did it!!")
+
+
+print("TEST 중123")
+print("MW Brench")
+print("Change")
+
+print("gdgdgdg")
